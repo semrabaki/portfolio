c1f193c738f2dee7ed6d701fd4846bc41d8ebe77
Semra Baki <usemrabaki@gmail.com>
2022-04-08 11:55:22 -0500

    updated

--- app-resume/src/utils/resumeData.js
+++ app-resume/src/utils/resumeData.js
@@ -31,7 +31,7 @@ export default{
 
 
     },
-    about: "Former researcher and teacher, current software engineer and life-long learner who has a huge passion for building software and exploring new technologies. Seeking to utilize broad educational background with excellent analytical, technical, and programming skills to meet business needs.\n \n My  skills:\n\n I have an experience in Spring Framework by using Spring Boot and Spring JWT.\n\n-Experience in using Java to create Restful-API in Spring environment. \n\n-I have experience building user interface with ReactJS along with React-router and Material-UI \n\n-Experience in using ReactJS with ES6 features to develop reusable components. \n\n-Experience in responsive web design using CSS3 media queries and libraries such as Bootstrap. \n\n-Experience in version control tools such as Git, GitHub.\n\n-Experience in using JIRA and following agile development process. \n\n-Deep understanding of Software Development Life Cycle (SDLC) including requirements analysis, design, testing implementation, and maintenance.\n\n-Deep understanding of Object-Oriented Programming (OOP) and Functional Programming (FP).\n\n-Excellent interpersonal abilities, communication skills, time management and team skills.",
+    about: "Former researcher and teacher, current software engineer and life-long learner who has a huge passion for building software and exploring new technologies. Seeking to utilize broad educational background with excellent analytical, technical, and programming skills to meet business needs.\n \n My  skills:\n\n -Have an experience in Spring Framework by using Spring Boot and Spring JWT.\n\n-Experience in using Java to create Restful-API in Spring environment. \n\n-I have experience building user interface with ReactJS along with React-router and Material-UI \n\n-Experience in using ReactJS with ES6 features to develop reusable components. \n\n-Experience in responsive web design using CSS3 media queries and libraries such as Bootstrap. \n\n-Experience in version control tools such as Git, GitHub.\n\n-Experience in using JIRA and following agile development process. \n\n-Deep understanding of Software Development Life Cycle (SDLC) including requirements analysis, design, testing implementation, and maintenance.\n\n-Deep understanding of Object-Oriented Programming (OOP) and Functional Programming (FP).\n\n-Excellent interpersonal abilities, communication skills, time management and team skills.",
     experinces:[
          {
              title:'Software Engineer',
@@ -39,7 +39,7 @@ export default{
              description:'Currently working as a Software Engineer utilizing technologies where we worked on national and international transactions to make payments on a secure system. Spring Tool Suite is used to develop the backend structure of this project. Spring Boot and Spring JWT is the framework on the backend to create secure, authenticated and authorized Restful APIs with the Maven project structure.I extended UserDetailsService and  I made connections to the Database which is PostgreSQL by extending JPA repository and build Spring powered applications. I am currently working on creating user interface with ReactJS along with React-router and reusable components with ES6 features. Many external libraries such as Bootstrap, Formik, Material-UI, Yup, React-strap, React-toastify help me to complete the UI part of the project. Git and GitHub are being used in my current team as a source control tool daily. Jira and Jira Xray are project management tools for our SCRUM team. I am currently working in an Agile environment and having Grooming meetings to understand user stories. Our Sprints are two weeks long and we are having daily standups. '
          },
          {
-            title:'Computer Science/Robotics/Turkish Teacher/ IT Specialist',
+            title:'Computer Science/Robotics Teacher/ IT Specialist',
             date:'August 2016 - June 2021',
             description:'Experience teaching programming basics and robotics classes for K-8th grade students. Also helped with IT related issues.  Joined weekly school meetings. Design morning and after-school extra-curricular technology programs such as coaching Robotics and  CyberPatriot team.  Incorporated technology into lessons to strengthen student understanding by utilizing the online textbook programs to assign lessons. I used many different software to connect the databases to enter students grades, take attendance, and submit lesson plans'
         },
@@ -47,21 +47,6 @@ export default{
             title:'Research Assistant',
             date:'2015 - 2016',
             description:'Helping course professors with technology related tasks such as hardware, modifying online courses.  Created an iBook for the Teacher Candidates.'
-        },
-        {
-            title:'Teaching Asistant',
-            date:'2007- 2009',
-            description:'I thought C, C++, and C# classes for undergraduate students.'
-        },
-        {
-            title:'Intern',
-            date:'June-September 2006',
-            description:'Barcode Reading (C#), Customer Following Program (C#): The three months program covered advanced topics in software engineering, SQL, Windows 2000 Server, and various Company Products.'
-        },
-        {
-            title:'Teaching Asistant',
-            date:'2005- 2007',
-            description:'Helping professor with teaching programing basics to first year undergrad students.'
         }
 
 
@@ -69,17 +54,14 @@ export default{
      educations:[
         {
             title:'Ph.D in Instructional Technology',
-            date:'2021',
             description:'My huge passion about learning lead me to start my Ph.D degree. During my Ph.D journey I took several Multimedia Production and Project Management classes. While I was working as a computer science teacher I realized that students reading levels differ and the most important factor on this differnce is students` current vocabulary knowledge. As I doctorate dissertation I designed a software that aims to teach students grade level vocabulary.'
         },
         {
            title:'MSCS in Computer Science',
-           date:'2009',
            description:'Right after my graduation from Computer Engineering, I started a master program in Computer Science at one of the valunrable university in Turkey.\nDuring my master I had took several valuable classes such as Data Mining, Pattern Recognition, Artificial Intelligence, Neural Network,Natural Language Processing, Advanced Operationg Systems,etc. I had also worked on four different projects that are recently published in international journals.'
        },
        {
            title:'BS in Computer Engineering',
-           date:'2007',
            description:'During my Bachelor Degree I took several enginering and advanced math classes. I also took algorithm, advanced algotirthm and database management classes.'
        }
 
